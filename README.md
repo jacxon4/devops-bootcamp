30eabda53721863f290849515edc22aca06e586b
canizarj <jacxon4@gmail.com>
2020-11-10 16:36:40 +0100

    Create both docker images and containers

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# devops-bootcamp
-Devops bootcamp exercises and deliverables
+# Devops bootcamp exercises
+
+Set of deliverables and exercises done during Devops bootcamp. Enter on each folder to see exercises related with that topic and follow readme instructions
--- containers/exercise1.sh
+++ containers/exercise1.sh
@@ -3,10 +3,34 @@
 # Assuming proper docker login already performed
 
 #To remove existing networks use next line 
-#docker network prune -f
+docker network prune -f
 
 #Create shared network
 docker network create lemoncode-challenge
 
-docker run -itd --name db --network lemoncode-challenge mongo
-#docker ps
+# Recreate container
+docker rm mongodb -f
+docker run -itd --name mongodb --network lemoncode-challenge mongo
+
+
+#Create backend image
+cd backend
+docker build . -t dotnet-backend
+
+# Recreate container
+docker rm dotnet-backend -f
+docker run -itd --name dotnet-backend -p 5000:5000 --network lemoncode-challenge dotnet-backend
+
+#Ensure container into newtork
+docker network inspect lemoncode-challenge | grep dotnet-backend
+cd ..
+
+#Create frontend image
+cd frontend
+docker build . -t react-frontend
+
+# Recreate container
+docker rm react-frontend -f
+docker run -itd --name react-frontend -p 3000:3000 --network lemoncode-challenge react-frontend
+cd ..
+
